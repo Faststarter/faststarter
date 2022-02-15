10f6f4fe5ea28a1269522f5ad236aa5bdce3f623
salemkode <salem.m.s2030@gmail.com>
2022-02-15 15:49:40 +0300

    Update utils.js

--- src/assets/utils.js
+++ src/assets/utils.js
@@ -20,8 +20,8 @@ let utils = {
   toWei(_value) {
     try {
       if (!_value) return 0;
-     _value = _value + "";
-      
+      _value = _value + "";
+
       return web3.utils.toWei(_value);
     } catch (error) {
       console.log("toWei function error : ");
