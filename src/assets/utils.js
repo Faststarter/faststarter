499afd7f92979b9daaea96d555dcfaf8dbc96211
salemkode <salem.m.s2030@gmail.com>
2022-02-15 10:52:28 +0300

    Support more than one in toWei function

--- src/assets/utils.js
+++ src/assets/utils.js
@@ -20,9 +20,8 @@ let utils = {
   toWei(_value) {
     try {
       if (!_value) return 0;
-      if (_value < 1) {
-        _value = _value + "";
-      }
+     _value = _value + "";
+      
       return web3.utils.toWei(_value);
     } catch (error) {
       console.log("toWei function error : ");
